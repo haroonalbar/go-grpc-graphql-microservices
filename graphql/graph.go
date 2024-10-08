c752c455608dd963afc5e9a3c1d1a30ca5dafa63
Haroon Albar <haroonalbarx@gmail.com>
2024-10-08 19:34:08 +0530

    new graphql server function

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,35 @@
 package main
+
+// type Server struct {
+// 	accountClient *account.Client
+// 	catalogClient *catalog.Client
+// 	orderClient   *order.Client
+// }
+
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	// catalogClient is dependant on accountClient
+	catalogClient, err := catalog.NewClient(catalogUrl)
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+
+	// orderClient is dependant on both clients above
+	orderClient, err := order.NewClient(orderUrl)
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return nil, err
+	}
+
+  return &Server{
+    accountClient
+    catalogClient
+    orderClient
+  }, nil
+}
