ff161c90ed429dbec9676e3ce207c41dde60b0c1
Haroon Albar <haroonalbarx@gmail.com>
2024-10-09 12:29:11 +0530

    populate cfg with envconfig and serve graphql server

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,39 +1,42 @@
 package main
 
+import "github.com/99designs/gqlgen/graphql"
+
 type Server struct {
 	// accountClient *account.Client
 	// catalogClient *catalog.Client
 	// orderClient   *order.Client
 }
 
-// func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
-// 	accountClient, err := account.NewClient(accountUrl)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	// catalogClient is dependant on accountClient
-// 	catalogClient, err := catalog.NewClient(catalogUrl)
-// 	if err != nil {
-// 		accountClient.Close()
-// 		return nil, err
-// 	}
-//
-// 	// orderClient is dependant on both clients above
-// 	orderClient, err := order.NewClient(orderUrl)
-// 	if err != nil {
-// 		accountClient.Close()
-// 		catalogClient.Close()
-// 		return nil, err
-// 	}
-//
-// 	return &Server{
-// 		accountClient,
-// 		catalogClient,
-// 		orderClient,
-// 	}, nil
-// }
-//
+func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	// accountClient, err := account.NewClient(accountUrl)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	//
+	// // catalogClient is dependant on accountClient
+	// catalogClient, err := catalog.NewClient(catalogUrl)
+	// if err != nil {
+	// 	accountClient.Close()
+	// 	return nil, err
+	// }
+	//
+	// // orderClient is dependant on both clients above
+	// orderClient, err := order.NewClient(orderUrl)
+	// if err != nil {
+	// 	accountClient.Close()
+	// 	catalogClient.Close()
+	// 	return nil, err
+	// }
+	//
+	// return &Server{
+	// 	accountClient,
+	// 	catalogClient,
+	// 	orderClient,
+	// }, nil
+	return nil, nil
+}
+
 // func (s *Server) Mutation() MutationResolver {
 // 	// mutation_resolver.go
 // 	return &mutationResolver{
@@ -54,10 +57,12 @@ type Server struct {
 // 		server: s,
 // 	}
 // }
+
 // // ToExecutableSchema is converting the Server instance into an executable GraphQL schema.
-// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
-// 	return NewExecutableSchema(Config{
-// 		// server matches ResolverRoot interface in graphql
-// 		Resolvers: s,
-// 	})
-// }
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	// return NewExecutableSchema(Config{
+	// 	// server matches ResolverRoot interface in graphql
+	// 	Resolvers: s,
+	// })
+	panic("")
+}
--- graphql/main.go
+++ graphql/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
+)
+
+// The envconfig tags you see in this Go struct definition are part of a popular
+// Go library called "envconfig" (or sometimes "go-envconfig").
+// This library is used to populate struct fields from environment variables.
+// Here's what these tags do:
+// - They map environment variables to struct fields.
+// - They allow you to specify the names of the environment variables that should be used to populate each field.
+type AppConfig struct {
+	AccountUrl string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogUrl string `envconfig:"CATALOG_SERVICE_URL"`
+	OrderUrl   string `envconfig:"ORDER_SERVICE_URL"`
+}
+
+func main() {
+	var cfg AppConfig
+	// Process populates the specified struct based on environment variables
+	// TODO:
+	// - envconfig no longer mainted change the implementation
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatalf("Error populating env : %v", err)
+	}
+
+	// graph.go
+	// Create a Graphql server
+	s, err := NewGraphQLServer(cfg.AccountUrl, cfg.CatalogUrl, cfg.OrderUrl)
+	if err != nil {
+		log.Fatalf("Error setting Graphql server: %v", err)
+	}
+
+	http.Handle("/graphql", handler.GraphQL(s.ToExecutableSchema()))
+	// graphql has a playground ui to run queries and mutations
+	http.Handle("/playground", handler.Playground("play", "/graphql"))
+}
