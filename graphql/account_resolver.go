f33ad04556d516cf6c067fa721de1d2b0eaae983
Haroon Albar <haroonalbarx@gmail.com>
2024-10-09 09:00:56 +0530

    all query resolvers

--- graphql/account_resolver.go
+++ graphql/account_resolver.go
@@ -4,4 +4,6 @@ type accountResolver struct {
 	server *Server
 }
 
-// Orders
+// // Orders are in accountResolver cause it's dependent on account
+// func (r *accountResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+// }
