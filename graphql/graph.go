18e0f1f4180e4e2a63c50d0ef684dbe6665eed3d
Haroon Albar <haroonalbarx@gmail.com>
2024-10-09 09:01:09 +0530

    comment out all graphql

--- graphql/graph.go
+++ graphql/graph.go
@@ -6,52 +6,58 @@ type Server struct {
 	// orderClient   *order.Client
 }
 
-func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
-	accountClient, err := account.NewClient(accountUrl)
-	if err != nil {
-		return nil, err
-	}
-
-	// catalogClient is dependant on accountClient
-	catalogClient, err := catalog.NewClient(catalogUrl)
-	if err != nil {
-		accountClient.Close()
-		return nil, err
-	}
-
-	// orderClient is dependant on both clients above
-	orderClient, err := order.NewClient(orderUrl)
-	if err != nil {
-		accountClient.Close()
-		catalogClient.Close()
-		return nil, err
-	}
-
-	return &Server{
-		accountClient,
-		catalogClient,
-		orderClient,
-	}, nil
-}
-
-func (s *Server) Mutation() MutationResolver {
-	// mutation_resolver.go
-	return &mutationResolver{
-		server: s,
-	}
-}
-
-func (s *Server) Query() QueryResolver {
-	// query_resolver.go
-	return &queryResolver{
-		server: s,
-	}
-}
-
-func (s *Server) Account() AccountResolver {
-	// account_resolver.go
-	return &accountResolver{
-		server: s,
-	}
-}
-
+// func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+// 	accountClient, err := account.NewClient(accountUrl)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	// catalogClient is dependant on accountClient
+// 	catalogClient, err := catalog.NewClient(catalogUrl)
+// 	if err != nil {
+// 		accountClient.Close()
+// 		return nil, err
+// 	}
+//
+// 	// orderClient is dependant on both clients above
+// 	orderClient, err := order.NewClient(orderUrl)
+// 	if err != nil {
+// 		accountClient.Close()
+// 		catalogClient.Close()
+// 		return nil, err
+// 	}
+//
+// 	return &Server{
+// 		accountClient,
+// 		catalogClient,
+// 		orderClient,
+// 	}, nil
+// }
+//
+// func (s *Server) Mutation() MutationResolver {
+// 	// mutation_resolver.go
+// 	return &mutationResolver{
+// 		server: s,
+// 	}
+// }
+//
+// func (s *Server) Query() QueryResolver {
+// 	// query_resolver.go
+// 	return &queryResolver{
+// 		server: s,
+// 	}
+// }
+//
+// func (s *Server) Account() AccountResolver {
+// 	// account_resolver.go
+// 	return &accountResolver{
+// 		server: s,
+// 	}
+// }
+//
+// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+// 	return NewExecutableSchema(Config{
+// 		// server matches ResolverRoot interface in graphql
+// 		Resolvers: s,
+// 	})
+// }
