05a566d37ad90b5768286d95b53b6463909b28ea
Haroon Albar <haroonalbarx@gmail.com>
2024-11-03 17:32:33 +0530

    feat[catalog]: generate pb from proto

--- catalog/server.go
+++ catalog/server.go
@@ -1,1 +1,9 @@
+//go:generate protoc --go_out=. --go-grpc_out=. catalog.proto
 package catalog
+
+import "github.com/haroonalbar/go-grpc-graphql-microservices/catalog/pb"
+
+type grpcServer struct {
+	pb.UnimplementedProductServiceServer
+	service Service
+}
