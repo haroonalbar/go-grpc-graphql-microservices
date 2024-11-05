fa1705e46bc574b9616ad45d30795cdd06afd4f5
Haroon Albar <haroonalbarx@gmail.com>
2024-11-05 15:48:35 +0530

    feat[catalog]: client done

--- catalog/client.go
+++ catalog/client.go
@@ -11,7 +11,8 @@ import (
 // As per the flow graphql to client to server to service to repository to db
 // So this Client is used by graphql to intact with catalog Microservice
 type Client struct {
-	conn    *grpc.ClientConn
+	conn *grpc.ClientConn
+	// from generated pb not from catalog
 	service pb.CatalogServiceClient
 }
 
@@ -33,3 +34,60 @@ func (c *Client) Close() {
 	c.conn.Close()
 }
 
+func (c *Client) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	res, err := c.service.PostProduct(
+		ctx,
+		&pb.PostProductRequest{
+			Name:        name,
+			Description: description,
+			Price:       price,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return &Product{
+		ID:          res.Product.Id,
+		Name:        res.Product.Name,
+		Description: res.Product.Description,
+		Price:       res.Product.Price,
+	}, nil
+}
+
+func (c *Client) GetProduct(ctx context.Context, id string) (*Product, error) {
+	res, err := c.service.GetProduct(
+		ctx,
+		&pb.GetProductRequest{
+			Id: id,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return &Product{
+		ID:          res.Product.Id,
+		Name:        res.Product.Name,
+		Description: res.Product.Description,
+		Price:       res.Product.Price,
+	}, nil
+}
+
+func (c *Client) GetProducts(ctx context.Context, skip, take uint64, ids []string, query string) ([]Product, error) {
+	res, err := c.service.GetProducts(ctx, &pb.GetProductsRequest{
+		Ids:   ids,
+		Skip:  skip,
+		Take:  take,
+		Query: query,
+	})
+	if err != nil {
+		return nil, err
+	}
+	var products []Product
+	for _, p := range res.Products {
+		products = append(products, Product{
+			ID:          p.Id,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		})
+	}
+	return products, nil
+}
