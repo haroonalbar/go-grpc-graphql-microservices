349ae30b00667c38eb557c49260650cbdf4d9c51
Haroon Albar <haroonalbarx@gmail.com>
2024-11-07 12:10:58 +0530

    feat[order]: service done

--- order/service.go
+++ order/service.go
@@ -3,6 +3,8 @@ package order
 import (
 	"context"
 	"time"
+
+	"github.com/segmentio/ksuid"
 )
 
 type Order struct {
@@ -34,10 +36,24 @@ func NewService(r Repository) Service {
 	return &orderService{r}
 }
 
-func (s *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
-	panic("")
+func (s orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
+	o := Order{
+		ID:        ksuid.New().String(),
+		CreatedAt: time.Now().UTC(),
+		AccountID: accountID,
+		Products:  products,
+	}
+	for _, p := range products {
+		// by default it would be 0.0
+		o.TotalPrice += p.Price * float64(p.Quantity)
+	}
+	err := s.repository.PutOrder(ctx, o)
+	if err != nil {
+		return nil, err
+	}
+	return &o, nil
 }
 
-func (s *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
-	panic("")
+func (s orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	return s.repository.GetOrderForAccount(ctx, accountID)
 }
