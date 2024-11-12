a27e7182d7fc25cf314feee8a0ffab74825e0d9a
Haroon Albar <haroonalbarx@gmail.com>
2024-11-12 11:04:15 +0530

    wip[order]:client setup new client and close

--- order/client.go
+++ order/client.go
@@ -1,1 +1,39 @@
 package order
+
+import (
+	"log"
+
+	"github.com/haroonalbar/go-grpc-graphql-microservices/order/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.OrderServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	// NOTE: used NewClient instead of depricated Dial
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	c := pb.NewOrderServiceClient(conn)
+	return &Client{conn: conn, service: c}, nil
+}
+
+func (c *Client) Close() {
+	err := c.conn.Close()
+	if err != nil {
+		log.Println("Error closing grpc connection : ", err)
+	}
+}
+
+func (c *Client) PostOrder() {
+	panic("")
+}
+
+func (c *Client) GetOrderForAccount() {
+	panic("")
+}
